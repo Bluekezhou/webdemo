49486f9760a2cea9b64dc320b373ce389b91375e
Bluecake <bluekezhou@qq.com>
2022-04-20 09:28:57 +0800

    add command inject

--- src/main/java/com/example/demo/controller/Login.java
+++ src/main/java/com/example/demo/controller/Login.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+
 @Controller
 public class Login {
     @RequestMapping(value = "/fastjson", method = RequestMethod.POST)
@@ -19,6 +21,22 @@ public class Login {
         result.put("code", 200);
         result.put("message", "success");
         result.put("data", "Hello " + obj.get("name"));
+        return result;
+    }
+
+    @RequestMapping(value = "/command", method = RequestMethod.POST)
+    @ResponseBody
+    public JSONObject run(@RequestBody String data) {
+        // JSONObject obj = JSON.parseObject(data, Feature.SupportNonPublicField); // 当使用 TemplatesImpl的时候用这个
+        JSONObject result = new JSONObject();
+        result.put("code", 200);
+        result.put("message", "success");
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec(new String[]{"cmd", "/c", data});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return result;
     }
 }
